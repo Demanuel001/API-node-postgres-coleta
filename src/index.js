a13ba4734f1a674ef26d86336b9839a2f1f0cd57
Demanuel001 <daniloemanuel360@gmail.com>
2023-04-26 11:29:49 -0300

    rota para update loja

--- src/index.js
+++ src/index.js
@@ -10,11 +10,21 @@ app.use(express.json());
 app.get('/', (req, res) =>{console.log('raiz')});
 app.get('/lojas', async (req,res) => {
     try{
-      const { rows } = await pool.query('SELECT * FROM coletagem');
+      const { rows } = await pool.query('SELECT * FROM lojas');
       return res.status(200).send(rows);
     } catch(err){
       return res.status(400).send(err);
     }
+});
+
+app.get('/coleta/:id_loja', async (req, res)=>{
+  const { id_loja } = req.params;
+  try{
+    const dadosIdLoja = await pool.query('SELECT * FROM coletagem WHERE id_loja = ($1)', [id_loja]);
+    return res.status(200).send(dadosIdLoja.rows);
+  }catch(err){
+    return res.status(400).send(err);
+  }
 });
 
 app.post('/login', async (req, res) => {
@@ -26,22 +36,32 @@ app.post('/login', async (req, res) => {
       name = await pool.query('INSERT INTO lojas(nome) VALUES ($1) RETURNING *', [storeName]);
     }
     return res.status(200).send(name.rows);
-  } catch(err){
+  }catch(err){
     return res.status(400).send(err);
   }
 });
 
 app.post('/coleta/:id', async (req, res) => {
   const {codbar, valor} = req.body;
-  const { id } = req.params
+  const { id } = req.params;
   try{
     const newData = await pool.query('INSERT INTO coletagem (codbar, valor, id_loja) VALUES ($1, $2, $3) RETURNING *', [codbar, valor, id]);
     return res.status(200).send(newData.rows);
-  } catch(err){
+  }catch(err){
     return res.status(400).send(err);
   }
 });
 
-
-
+app.patch('/lojas/:user_id/:loja_id', async (req, res) =>{
+  const { user_id, loja_id } = req.params;
+  const data = req.body;
+  try{
+    // const permissaoUsuario = await pool.query('SELECT * FROM usuarios WHERE id = ($1) AND adm = ($2)', [user_id, true]);
+    // if(!permissaoUsuario.rows[0]) return res.status(400).send('Usuário sem permissão.');
+    const updateLoja = await pool.query('UPDATE lojas SET nome = ($1), endereco = ($2) WHERE id = ($3) RETURNING *', [ data.nome, data.endereco, loja_id ]);
+    return res.status(200).send(updateLoja.rows);
+  }catch(err){
+    return res.status(400).send(err);
+  }
+})
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
